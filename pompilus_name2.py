e7cdcff28de8c05cc9dc765fbb8906c5a5495fad
jr23ps <112111494+jr23ps@users.noreply.github.com>
2023-01-19 13:17:42 -0600

    Add files via upload

---
+++ pompilus_name2.py
@@ -0,0 +1,91 @@
+from turtle import *
+canvas = Screen()
+canvas.setup (600,400)
+smith = Turtle ()
+smith.shape ("turtle")
+smith.color ("white")
+smith.forward (50)
+smith.left (90)
+smith.color("red")
+smith.right(90)
+smith.right(90)
+smith.forward(90)
+smith.left(150)
+smith.forward(90)
+smith.left(100)
+smith.forward(50)
+smith.right(180)
+smith.forward(50)
+
+
+from turtle import *
+canvas = Screen()
+canvas.setup (600,400)
+smith = Turtle ()
+smith.shape ("turtle")
+smith.color ("white")
+smith.forward (50)
+smith.right (90)
+smith.color("green")
+smith.left(90)
+smith.left(90)
+smith.forward(90)
+smith.right(150)
+smith.forward(90)
+smith.right(100)
+smith.forward(50)
+smith.left(180)
+smith.forward(50)
+
+
+
+from turtle import *
+canvas = Screen()
+canvas.setup (600,400)
+smith = Turtle ()
+smith.shape ("turtle")
+smith.color ("white")
+smith.forward (50)
+smith.left (90)
+smith.color("yellow")
+smith.right(90)
+smith.left(90)
+smith.forward(90)
+smith.left(150)
+smith.forward(90)
+smith.left(100)
+smith.forward(50)
+smith.right(180)
+smith.forward(50)
+
+from turtle import *
+canvas = Screen()
+canvas.setup (600,400)
+smith = Turtle ()
+smith.shape ("turtle")
+smith.color ("white")
+smith.forward (50)
+smith.right (90)
+smith.color("blue")
+smith.left(90)
+smith.right(90)
+smith.forward(90)
+smith.right(150)
+smith.forward(90)
+smith.right(100)
+smith.forward(50)
+smith.right(180)
+smith.forward(50)
+
+
+
+
+
+
+
+
+
+
+
+
+
