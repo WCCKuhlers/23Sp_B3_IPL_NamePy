fe70856edf4cbfa92b890e3b135335fa42bfe7fd
TheBlayze <79122819+TheBlayze@users.noreply.github.com>
2023-01-19 13:18:28 -0600

    Add files via upload

---
+++ williams_name.py
@@ -0,0 +1,9 @@
+from turtle import *
+canvas = Screen()
+canvas.setup(1000,1000)
+blayze = Turtle()
+blayze.shape("turtle")
+blayze.color("blue")
+for x in range(365):
+    blayze.forward(2)
+    blayze.left(1)
